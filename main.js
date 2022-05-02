40dc8786c90d176f9b368955d8532dee099313b3
Justin Macak <justin.macak@gmail.com>
2022-05-02 15:00:51 -0400

    I added functions

--- main.js
+++ main.js
@@ -3,3 +3,22 @@ var ctx = can.getContext("2d");
 
 can.width = 100;
 can.height = 100;
+
+var selectedObj = document.getElementById("pancakeOrder");
+
+function hideObject(selectedObj) {
+    selectedObj.style.visibility = "hidden";
+}
+
+function showObject(selectedObj) {
+    selectedObj.style.visibility = "visible";
+}
+
+function toggleVisibility(selectedObj) {
+    if (selectedObj.style.visibility == "hidden") {
+        selectedObj.style.visibility = "visible";
+    }
+    else {
+        selectedObj.style.visibility = "hidden";
+    }
+}
