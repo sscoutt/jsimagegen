8da7ed15936536523a8e2c36618c0d1903874ec2
Justin Macak <justin.macak@gmail.com>
2022-05-03 17:05:20 -0400

    broke it :(

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ var ctx = can.getContext("2d");
 can.width = 100;
 can.height = 100;
 
-var selectedObj = document.getElementById("pancakeOrder");
+var pancakes = document.getElementById("pancakeOrder");
+var floofBtn = document.getElementById("floofBtn");
+var floofFlooferBtn = document.getElementById("floof-FlooferBtn");
 
 // Visibility functions
 function hideObject(selectedObj) {
@@ -22,4 +24,19 @@ function toggleVisibility(selectedObj) {
     else {
         selectedObj.style.visibility = "hidden";
     }
+}
+
+//Color functions
+function changeColor(selectedObj) {
+    selectedObj.style.color = "#000000";
+}
+
+function setSelectedObj(id) {
+    var selectedObj = document.getElementById(id);
+    alert(`selected ${selectedObj.textContent}!`);
+    testScope();
+}
+
+function testScope() {
+    alert(`Scope test: selectedObj is ${selectedObj.textContent}`);
 }
