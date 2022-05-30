78f8eeb2bd82a13a21a027348388f93b35dc604c
SebastianH2000 <sebastianmhunt@gmail.com>
2022-05-30 09:43:02 -0400

    Adding HTML Canvas Stuffs

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
 var can = document.getElementById("imageDisplay");
 var ctx = can.getContext("2d");
 
-can.width = 100;
-can.height = 100;
+var canWidth = 600;
+var canHeight = 600;
+
+can.width = canWidth;
+can.height = canHeight;
 
 // var selectedObj = document.getElementById("pancakeOrder");
 var floofBtn = document.getElementById("floofBtn");
 var floofFlooferBtn = document.getElementById("floof-FlooferBtn");
 var yummyPancakes = document.getElementById("yummyPancakes");
-var strawberries = document.getElementById("strawberries")
+var strawberries = document.getElementById("strawberries");
 
 // Visibility functions
 
-yummyPancakes.style.visibility = "hidden"
-strawberries.style.visibility = "hidden"
+yummyPancakes.style.visibility = "hidden";
+strawberries.style.visibility = "hidden";
 
 function hideObject(selectedObj) {
     selectedObj.style.visibility = "hidden";
@@ -44,19 +47,40 @@ function setSelectedObj(id) {
 
 function toggleSize(selectedObj) {
     if (selectedObj.style.height == "30px") {
-        (selectedObj.style.height = "300px")
+        (selectedObj.style.height = "300px");
     }
     else {
-        (selectedObj.style.height = "30px")
+        (selectedObj.style.height = "30px");
     }
 }
 
 function strawberryPancakes() {
-    yummyPancakes.style.visibility = "hidden"
-    strawberries.style.visibility = "visible"
+    yummyPancakes.style.visibility = "hidden";
+    strawberries.style.visibility = "visible";
 }
 
 function noStrawberries() {
-    yummyPancakes.style.visibility = "visible"
-    strawberries.style.visibility = "hidden"
+    yummyPancakes.style.visibility = "visible";
+    strawberries.style.visibility = "hidden";
 }
+
+
+
+
+
+function clearCanvas() {
+    ctx.clearRect(0,0,canWidth,canHeight);
+}
+
+//canvas magic
+var pancakeImg = document.getElementById("pancakeOrder");
+
+var loopInterval = 50;
+var mainTimer = 0;
+function mainLoop() {
+    mainTimer += loopInterval/1000;
+    clearCanvas();
+    ctx.drawImage(pancakeImg,10,10,400+Math.sin(mainTimer)*100,400+Math.cos(mainTimer)*100);
+}
+
+setInterval(mainLoop,1000/loopInterval);
